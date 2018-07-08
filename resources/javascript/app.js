eb24dd73615a8e7571616c9fe2b8a3a5d3d7e0c6
Brant Han <brant.han@gmail.com>
2018-07-08 00:01:09 -0700

    add css and more functions

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -1,3 +1,4 @@
+var APPID = "6a45c48e46201ad932cb29b2f1863923";
 var temp;
 var loc;
 var icon;
@@ -5,14 +6,42 @@ var humidity;
 var wind;
 var direction;
 
+function updateByZip(zip){
+   var url = "http://api.openweathermap.org/data/2.5/weather?" + "zip=" + zip + "&APPID=" + APPID;
+   sendRequest(url);
+}
 
-function update(weather){
+function sendRequest(url){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+   if (this.readyState == 4 && this.status == 200) {
+       // Action to be performed when the document is read;
+       var data = JSON.parse(xhttp.responseText);
+       var newWeather = {};
+       newWeather.temp = data.main.temp;
+       newWeather.loc = data.name;
+       newWeather.icon = data.weather[0].id;
+       newWeather.humidity = data.main.humidity;
+       newWeather.wind = data.wind.speed;
+       newWeather.direction = data.wind.deg;
+      
+       updateFrontEnd(newWeather);
+       
+    }
+  };
+  //above is a library, so we call via open/ send the newWeather data, along with url with api and send.
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function updateFrontEnd(weather){
   temp.innerHTML = weather.temp;
   loc.innerHTML = weather.loc;
+   icon.src = "./resources/images/codes/" + weather.icon + ".png";
    humidity.innerHTML = weather.humidity;
   wind.innerHTML = weather.wind;
   direction.innerHTML = weather.direction;
- icon.src = "./images/codes/" + weather.icon + ".png";
+
 }
 
 window.onload = function () {
@@ -23,13 +52,15 @@ window.onload = function () {
   wind = document.getElementById ("wind");
   direction = document.getElementById ("direction");
   
-  var weather = {};
-  weather.wind = 3.5;
-  weather.direction = "N";
-  weather.humidity = 35;
-  weather.loc = "Boston";
-  weather.temp = "45";
-  weather.icon = 200;
+  var weather2 = {};
+  weather2.wind = 3.5;
+  weather2.direction = "N";
+  weather2.humidity = 35;
+  weather2.loc = "Boston";
+  weather2.temp = "45";
+  weather2.icon = 200;
+  
+  updateByZip(88901);
 }
 
 
